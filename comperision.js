c475665706c6619147b07aa87237272b07b2ccd8
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-10-06 23:27:06 +0530

    comperision

---
+++ comperision.js
@@ -0,0 +1,31 @@
+//basic operation
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==2);
+// console.log(2!=1);
+
+// console.log("02">1);
+// console.log(02>1);
+
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null<0);
+// console.log(null<=0);
+// console.log(null==0);
+
+// console.log(null>1);
+// console.log(null>=1);
+// console.log(null<1);
+// console.log(null<=1);
+// console.log(null==1);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// ===
+
+console.log("2"===2);
